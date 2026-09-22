48cdc50d9b59f24fc087d8bc2fc3bee83486e938
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-09-22 23:38:05 +0530

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["MERN Stack Developer", "Open Source Contributor", "DSA Learner", "Problem Solver", "Competitive Programming Learner"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
